c42f1134ad2183cc6c46b4e171a4fc595438b2c7
Jonas Kjellerup <jonas@hextech.dk>
2021-11-04 14:47:16 +0100

    Added database sets to context

--- Flea/Models/BingoContext.cs
+++ Flea/Models/BingoContext.cs
@@ -4,6 +4,16 @@ namespace Flea.Models
 {
     public class BingoContext : DbContext
     {
+        public DbSet<Event> Events { get; set; }
+        
+        public DbSet<Cluster> Clusters { get; set; }
+        
+        public DbSet<Table> Tables { get; set; }
+        
+        public DbSet<Reservation> Reservations { get; set; }
+        
+        public DbSet<Customer> Customers { get; set; }
+
         public BingoContext(DbContextOptions<BingoContext> contextOptions) : base(contextOptions)
         {
             
