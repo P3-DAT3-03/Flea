dbcf7c097f26503f316827eac93cf56bc5198a64
pimber <christofferctj@gmail.com>
2021-11-10 11:59:40 +0100

    Har lavet rettelser

--- Flea/Models/Customer.cs
+++ Flea/Models/Customer.cs
@@ -5,9 +5,9 @@
 		public int Id { get; set; }
 
 		public string Name { get; set; }
-		public int PhoneNumber { get; set; }
+		public string PhoneNumber { get; set; }
 
-		public Customer(string name, int phoneNumber)
+		public Customer(string name, string phoneNumber)
 		{
 			Name = name;
 			PhoneNumber = phoneNumber;
