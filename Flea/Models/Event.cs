e99c1ec5e9bca80b00ac0a130d07f6ec9979a1d0
Jonas Kjellerup <jonas@hextech.dk>
2021-11-10 12:29:49 +0100

    Reverted Event.cs

--- Flea/Models/Event.cs
+++ Flea/Models/Event.cs
@@ -27,41 +27,6 @@ namespace Flea.Models
         }
 
 
-        public int TablesNotPlaced => Tables.Length - ReservationCount;
-        public bool VerifyPlacements => throw new NotImplementedException();
-        public bool AssignReservationToTables => throw new NotImplementedException();
-    }
-
-    public class Table
-    {
-        public enum TableType
-        {
-            table,
-            empty,
-            rack,
-        }
-
-        public Reservation TableReservation { get; set; }
-        public TableType Type { get; set; }
-
-        public Table()
-        {
-            Type = TableType.table;
-        }
-    }
-
-    public class Reservation
-    {
-        public int Priority { get; set; }
-        public int TableCount { get; set; }
-        public bool Paid { get; set; }
-        public string Comments { get; set; }
-        public List<Table> Tables { get; } = new List<Table>();
-    }
-
-    public class Customer
-    {
-        public string Name { get; set; }
-        public int PhoneNumber { get; set; }
+        public int ComputeRemainingTables => Clusters.Aggregate(0, (acc, cluster) => acc + cluster.TablesNotPlaced);
     }
 }
