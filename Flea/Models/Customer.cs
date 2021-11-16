c093952cb60aa87c59b50e1db3fdb1c757e6e80a
pimber <christofferctj@gmail.com>
2021-11-16 20:08:50 +0100

    ...

--- Flea/Models/Customer.cs
+++ Flea/Models/Customer.cs
@@ -1,23 +1,43 @@
-﻿
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace Flea.Models
 {
-	public class Customer
-	{
-		public int Id { get; set; }
+    public class Customer : IModelEntity<Customer, BingoContext>
+    {
+        public int Id { get; set; }
 
-		public string Name { get; set; }
-		public string PhoneNumber { get; set; }
+        [StringLength(64)]
+        public string Name { get; set; }
+		
+        [StringLength(8)]
+        [Column(TypeName = "VARCHAR")]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
 
-		public Customer(string name, string phoneNumber)
-		{
-			Name = name;
-			PhoneNumber = phoneNumber;
-		}
+        public Customer() : this(0) {}
+		
+        public Customer(int id)
+        {
+            Id = id;
+            Name = string.Empty;
+            PhoneNumber = string.Empty;
+        }
+		
+        public Customer(string name, string phoneNumber)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+        }
 
-		public Reservation CreateReservation(int priority, int tables, bool paid, string comments)
-		{
-			return new Reservation(priority, tables, paid, comments, this);
-		}
-	}
+        public Reservation CreateReservation(int priority, int tables, bool paid, string comments)
+        {
+            return new Reservation(priority, tables, paid, comments, this);
+        }
+        public Customer Clone() => (Customer) this.MemberwiseClone();
+		
+        DbSet<Customer> IModelEntity<Customer, BingoContext>.GetDbSet(BingoContext ctx) => ctx.Customers;
+    }
 }
